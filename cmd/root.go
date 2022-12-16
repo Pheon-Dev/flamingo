f08c262ab3500edb60ef52bcfdcee3fbe8cbb4b9
Pheon-Dev <devpheon@gmail.com>
2022-12-16 23:29:09 +0300

    chore: Update, View, Init

--- cmd/root.go
+++ cmd/root.go
@@ -11,26 +11,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-var (
-	cfgFile       string
-	userLicense   string
-	docStyle      = lipgloss.NewStyle().Padding(1, 2)
-	quitTextStyle = lipgloss.NewStyle().Padding(1, 2)
-	itemStyle     = lipgloss.NewStyle().PaddingLeft(1)
-	titleStyle    = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("#c0caf5")).
-			Background(lipgloss.Color("#ff79c6")).
-			Padding(0, 1)
-	rootCmd = &cobra.Command{
-		Use:   "flamingo",
-		Short: "Swift Configurations & Projects File Navigator",
-		Long:  `Switch smoothly between different file configurations and projects without ever needing to cd into each individual file location`,
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Flamingoes to town")
-		},
-	}
-)
-
 type item struct {
 	title       string
 	description string
@@ -61,11 +41,32 @@ func editor(path string) tea.Cmd {
 	return nil
 }
 
-func Execute() error {
-	return rootCmd.Execute()
+func (m model) Init() tea.Cmd {
+	return nil
 }
 
-func init() {
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		height, width := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-width, msg.Height-height)
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "escape":
+			return m, tea.Quit
+		}
+	}
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return "\n" + m.list.View()
+}
+
+func (m model) init() {
 	cobra.OnInitialize(initConfig)
 
 	rootCmd.PersistentFlags().
@@ -85,6 +86,102 @@ func init() {
 	rootCmd.AddCommand(versionCmd)
 }
 
+var (
+	cfgFile       string
+	userLicense   string
+	docStyle      = lipgloss.NewStyle().Padding(1, 2)
+	quitTextStyle = lipgloss.NewStyle().Padding(1, 2)
+	itemStyle     = lipgloss.NewStyle().PaddingLeft(1)
+	titleStyle    = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#c0caf5")).
+			Background(lipgloss.Color("#ff79c6")).
+			Padding(0, 1)
+	rootCmd = &cobra.Command{
+		Use:   "flamingo",
+		Short: "Swift Configurations & Projects File Navigator",
+		Long:  `Switch smoothly between different file configurations and projects without ever needing to cd into each individual file location`,
+		Run: func(cmd *cobra.Command, args []string) {
+			projects := []list.Item{
+				item{title: "nvim", description: "$HOME/.config/nvim"},
+				item{
+					title:       "flamingo",
+					description: "$HOME/Documents/go/src/github.com/Pheon-Dev/flamingo",
+				},
+				item{title: "dwm", description: "$HOME/.config/arco-dwm"},
+				item{title: "zsh", description: "$HOME/.config/zsh"},
+				item{title: "dmenu", description: "$HOME/.config/dmenu"},
+				item{title: "btop", description: "$HOME/.config/btop"},
+				item{title: "tmux", description: "$HOME/.tmux"},
+				item{
+					title:       "st Simple Terminal",
+					description: "$HOME/.config/arco-st",
+				},
+				item{
+					title:       "lazygit",
+					description: "$HOME/.config/lazygit",
+				},
+				item{
+					title:       "ranger",
+					description: "$HOME/.config/ranger",
+				},
+				item{
+					title:       "fm file manager",
+					description: "$HOME/.config/fm",
+				},
+				item{title: "moc", description: ".moc"},
+				item{
+					title:       "p app",
+					description: "$HOME/Documents/go/src/github.com/Pheon-Dev/p",
+				},
+				item{
+					title:       "neovim",
+					description: "$HOME/Documents/Neovim",
+				},
+				item{
+					title:       "class",
+					description: "$HOME/Documents/CMT",
+				},
+				item{
+					title:       "go",
+					description: "$HOME/Documents/go/src/github.com/Pheon-Dev",
+				},
+				item{
+					title:       "bubbletea",
+					description: "$HOME/Documents/go/git/bubbletea/examples",
+				},
+				item{
+					title:       "go apps",
+					description: "$HOME/Documents/go/git",
+				},
+				item{
+					title:       "destiny",
+					description: "$HOME/Documents/NextJS/App/destiny-credit",
+				},
+				item{
+					title:       "devlen",
+					description: "$HOME/Documents/NextJS/App/devlen",
+				},
+				item{
+					title:       "typescript",
+					description: "$HOME/Documents/NextJS/App",
+				},
+			}
+
+			l := list.New(projects, list.NewDefaultDelegate(), 0, 0)
+			m := model{list: l}
+
+			if _, err := tea.NewProgram(m, tea.WithAltScreen()).Run(); err != nil {
+				fmt.Println("Error Running Program: ", err)
+				os.Exit(1)
+			}
+		},
+	}
+)
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
 func initConfig() {
 	if cfgFile != "" {
 		viper.SetConfigFile(cfgFile)
@@ -98,9 +195,10 @@ func initConfig() {
 
 		viper.AutomaticEnv()
 
-		if err := viper.ReadInConfig(); err != nil {
+		if verr := viper.ReadInConfig(); verr != nil {
 			fmt.Println("Using config file: ", viper.ConfigFileUsed())
 		}
+
 	}
 }
 
